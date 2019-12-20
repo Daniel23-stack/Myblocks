db3396359022911b7849bdf98a745cef96ed2cd6
Hannes Roos <hannes.roos@inspirze.com>
2019-12-20 11:40:13 +0200

    add parent post message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-froala-design-blocks';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.navigateByUrl("call-to-action")
+  }
 }
